a3ac1e8d0bec4beffa8328276d96226de51dcfea
Simon Rascovsky <simonmd@gmail.com>
2012-03-06 22:46:51 -0500

    escritura basica de png a partir del nifti

--- nifti_tests.rb
+++ nifti_tests.rb
@@ -1,10 +1,47 @@
 require "nifti"
-# Read file:
-obj = NIFTI::NObject.new("samples/brain.nii", :narray => true)
-# Display some key information about the file:
- # puts obj.header['pixdim']
-#  puts obj.header['scl_inter']
+require "hoe"
+require "inline"
 
-max = obj.image.max
-puts max
-#stddev = obj.image.stddev
+require 'png'
+
+class NArray
+  # returns all the indices to access the values of the NArray.  if start == 1,
+  # then the first dimension (row) values are not returned, if start == 2,
+  # then the first two dimensions are skipped.
+  #
+  # if a block is provided, the indices are yielded one at a time
+  # [obviously, this could be made into a fast while loop instead of
+  # recursive ... someone have at it]
+  def indices(start=0, ar_of_indices=[], final=[], level=shape.size-1, &block)
+    if level >= 0
+      (1...(shape[level])).each do |s|
+        new_indices = ar_of_indices.dup
+        new_indices.unshift(s)
+        if (new_indices.size == (shape.size - start))
+          block.call(new_indices)
+          final << new_indices 
+        end
+        indices(start, new_indices, final, level-1, &block)
+      end
+    end
+    final
+  end
+end
+
+# Read file:
+obj = NIFTI::NObject.new("samples/slice_76.nii", :narray => true)
+
+width = obj.header["dim"][1]
+height = obj.header["dim"][2]
+
+narr = obj.image
+
+canvas = PNG::Canvas.new(width, height)
+
+narr.indices do |n,m|
+  val=(narr[n,m])
+  canvas[m,n] = PNG::Color.new(val,val,val)
+end
+
+png = PNG.new canvas
+png.save 'test.png'
