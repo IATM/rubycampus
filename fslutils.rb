0ac39a15aadd9b010c40f50a45e50050fff19585
Simon Rascovsky <simonmd@gmail.com>
2012-03-23 16:54:50 -0500

    nicer pdf en una pagina

--- fslutils.rb
+++ fslutils.rb
@@ -36,7 +36,6 @@ puts options.inspect
 
 coord = `fslstats -t #{options[:stats]} -C`.split
 
-
 lh = {}
 rh = {}
 axis = ["x", "y", "z"]
@@ -241,18 +240,30 @@ Prawn::Document.generate('report.pdf') do |pdf|
   pdf.formatted_text [ { :text => "Patient name: ", :styles => [:bold], size: 10 }, { :text => PatientName, :styles => [:bold], size: 10 }]
   pdf.formatted_text [ { :text => "Patient ID: ", :styles => [:bold], size: 10 }, { :text => PatientID, size: 10 }]
   pdf.formatted_text [ { :text => "Patient Birthdate: ", :styles => [:bold], size: 10 }, { :text => PatientBirthdate, size: 10 }]
-  pdf.move_down 10
+  pdf.move_down 5
 
-  # SubTitle
+  # SubTitle RH
   pdf.text "Right Hippocampus" , size: 13, style: :bold, :align => :center
   pdf.move_down 5
   
-  # Images
-  pdf.image "axial.png", :width => 300, :height => 300, :position => 7
-  pdf.move_up 300
-  pdf.image "sagital.png", :width => 200, :height => 150, :position => 310
-  pdf.image "coronal.png", :width => 200, :height => 150, :position => 310
-  pdf.move_down 10
+  # Images RH
+  pdf.image "rh_axial.png", :width => 200, :height => 200, :position => 20
+  pdf.move_up 200
+  pdf.image "rh_sagital.png", :width => 150, :height => 100, :position => 210
+  pdf.image "rh_coronal.png", :width => 150, :height => 100, :position => 210
+  pdf.move_down 5
+  
+  # SubTitle LH
+  pdf.text "Left Hippocampus" , size: 13, style: :bold, :align => :center
+  pdf.move_down 5
+  
+  # Images LH
+  pdf.image "lh_axial.png", :width => 200, :height => 200, :position => 20
+  pdf.move_up 200
+  pdf.image "lh_sagital.png", :width => 150, :height => 100, :position => 210
+  pdf.image "lh_coronal.png", :width => 150, :height => 100, :position => 210
+  pdf.move_down 5
+  
   
   # Volumes Table
   pdf.table([ ["Right Hippocampus volume", "123.45"],
